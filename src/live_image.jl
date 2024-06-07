0f35fb2eea1e060a29eb45761179cf047d38d5f2
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2024-06-07 16:34:25 +0900

    Wavelength and time data work now

--- src/live_image.jl
+++ src/live_image.jl
@@ -7,7 +7,7 @@ in the given directory. Satellite panels can be opened via buttons.
 function live_image(datadir::String,
         load_function::Function=load_image,
         file_ext::String=".lvm";
-        wavelength = nothing,
+        wavelengths_file = nothing,
         waittime = 0.1,
         theme = nothing
     )
@@ -18,17 +18,19 @@ function live_image(datadir::String,
 
     fig = Figure(size = (600, 900))
 
-    img = Observable(rand(100, 100))
-    time = Observable(range(0, 100, length = size(to_value(img), 1)))
-    if wavelength !== nothing
-        λs = Observable(vec(readdlm(wavelength)))
-        img = Observable(rand(length(to_value(time)), length(to_value(λs))))
+    img = Observable(rand(200, 100))
+    time = Observable(collect(range(0, 100, length = size(to_value(img), 1))))
+    if wavelengths_file !== nothing
+        λs = Observable(vec(readdlm(wavelengths_file)))
+        img[] = rand(length(to_value(time)), length(to_value(λs)))
     else
-        λs = Observable(range(400, 800, length = size(to_value(img), 2)))
+        num_wavelengths = size(to_value(img), 2)
+        λs = Observable(collect(range(0, num_wavelengths, length = num_wavelengths)))
     end
-    λ_step = @lift($(λs)[2] - $(λs)[1])
-    y_line = Observable(700.0)
-    cut = Observable(to_value(img)[:, 1])
+    Δy = @lift(abs($λs[2] - $λs[1]))
+    y_line = Observable(to_value(λs)[1])
+    y_idx = Observable(1)
+    cut = @lift($img[:, $y_idx])
 
 
     sc = display(fig)
@@ -40,7 +42,12 @@ function live_image(datadir::String,
         tellwidth = false,
         )
     
-    ax1 = Axis(fig[1, 1][1, 1], xlabel = "Time (ps)", ylabel = "Wavelength (nm)")
+    ax1 = Axis(fig[1, 1][1, 1], 
+            xlabel = "Time (ps)", 
+            ylabel = "Wavelength (nm)",
+            xticks = LinearTicks(7),
+            yticks = LinearTicks(7),
+            )
 
     hm = heatmap!(time, λs, img)
     hlines!(y_line, color = :red)
@@ -48,48 +55,72 @@ function live_image(datadir::String,
     ax2 = Axis(fig[1, 1][2, 1],
         xlabel = "Time (ps)",
         ylabel = "Intensity (Arb.)",
-        yticklabelspace = 50.0
+        yticklabelspace = 50.0,
+        xticks = LinearTicks(7),
+        yticks = LinearTicks(7),
         )
-    line = lines!(ax2, cut, color = :firebrick3)
+    line = lines!(time, cut, color = :firebrick3)
     livetext = text!(" • Live", color = :red, space = :relative, align = (:left, :bottom))
 
     # Events
 
-    on(events(ax1).mouseposition) do mpos
-        y_line[] =  trunc(Int, mouseposition(ax1.scene)[2])
-        yaxis_limits = ax1.yaxis.attributes.limits[]
-
-        if y_line[] >= yaxis_limits[1] && y_line[] <= yaxis_limits[2]
-            y_idx = findfirst(isapprox(y_line[], atol = λ_step[]), λs[])
-            cut[] = to_value(img)[:, y_idx]
+    on(events(fig).mouseposition) do mpos
+        if is_mouseinside(ax1)
+            y_line[] =  trunc(Int, mouseposition(ax1.scene)[2])
+            y_idx[] = findfirst(isapprox(y_line[], atol = Δy[]), λs[])
+            autolimits!(ax2)
         end
-        autolimits!(ax2)
     end
 
     on(figbutton.clicks) do _
-        newfig = satellite_image(to_value(img), to_value(time), to_value(λs))
+        newfig = satellite_image(to_value(img), to_value(time), to_value(λs), ax1.title)
         display(GLMakie.Screen(), newfig)
+        DataInspector(newfig)
     end
 
     # Watch for new data
-    while true
-        (file, event) = watch_folder(datadir)
-        sleep(waittime)
-        
-        if endswith(file, file_ext)
+    found_files = Dict("CCD" => false, "T_" => false)
+    files = Dict("CCD" => "", "T_" => "")
 
+    while true
+        file, event = watch_folder(datadir)
+        sleep(waittime)
+
+        if isa(file, String) && file != "." && file != ".." && endswith(file, file_ext)
             if findfirst('\\', file) == 1
                 file = file[2:end]
             end
-            println("New file: ", file)            
+            
+            println("New file: ", file)
+        
+            if file[1:3] == "CCD"
+                found_files["CCD"] = true
+                files["CCD"] = joinpath(datadir, file)
+            elseif file[1:2] == "T_"
+                found_files["T_"] = true
+                files["T_"] = joinpath(datadir, file)
+            end
 
-            img[], filename = load_function(joinpath(datadir, file))
-            ax1.title = filename
+            if found_files["CCD"] && found_files["T_"]
+                img.val, filename = load_image(files["CCD"])
+                time.val = vec(readdlm(files["T_"], skipstart=1))
+                num_ypoints = size(to_value(img), 2)
+
+                if wavelengths_file !== nothing
+                    λs.val = vec(readdlm(wavelengths_file))
+                else
+                    λs.val = collect(range(0, num_ypoints, length = num_ypoints))
+                end
+                notify(img)
+                ax1.title = filename
+                found_files = Dict("CCD" => false, "T_" => false)
+            end
             autolimits!(ax1)
+            autolimits!(ax2)
         end
     end
-
 end
+
 
 
 """
@@ -98,37 +129,63 @@ end
 A satellite panel that appears upon clicking a button on the live panel.
 Not a user-facing function.
 """
-function satellite_image(img, time, λs)
+function satellite_image(img, time, λs, title)
 
-    fig = Figure()
-    DataInspector()
+    fig = Figure(size = (600, 900))
 
     # Observables and widgets
 
-    y_line = Observable(10.0)
-    cut = Observable(img[:, 1])
+    Δy = abs(λs[2] - λs[1])
+    y_line = Observable(λs[1])
+    y_idx = Observable(1)
+    cut = @lift(img[:, $y_idx])
     save_button = Button(fig, label = "Save as png")
     
-    # fig[2, 1] = vgrid!(
-    #     save_button;
-    #     tellwidth = false,
-    #     )
-    
-    ax1 = Axis(fig[1, 1][1, 1], xlabel = "Time (ps)", ylabel = "Wavelength (nm)")
+    fig[2, 1] = vgrid!(
+        save_button;
+        tellwidth = false,
+        )
 
-    hm = heatmap!(ax1, img)
+    ax1 = Axis(fig[1, 1][1, 1], 
+            title = title,
+            xlabel = "Time (ps)",
+            ylabel = "Wavelength (nm)",
+            xticks = LinearTicks(7),
+            yticks = LinearTicks(7),
+            )
+
+    hm = heatmap!(time, λs, img)
     hlines!(y_line, color = :red)
 
     ax2 = Axis(fig[1, 1][2, 1],
         xlabel = "Time (ps)",
         ylabel = "Intensity (Arb.)",
-        yticklabelspace = 50.0
+        yticklabelspace = 50.0,
+        xticks = LinearTicks(7),
+        yticks = LinearTicks(7),
         )
-    line = lines!(ax2, cut, color = :firebrick3)
+    line = lines!(time, cut)
 
-
-
-    on(events(ax1).mouseposition) do mpos
-        autolimits!(ax2)
+    on(events(fig).mouseposition) do mpos
+        if is_mouseinside(ax1)
+            y_line[] =  trunc(Int, mouseposition(ax1.scene)[2])
+            y_idx[] = findfirst(isapprox(y_line[], atol = Δy), λs)
+            autolimits!(ax2)
+        end
     end
+
+    on(savebutton.clicks) do _
+        save_folder = "./plots/"
+        if !isdir(save_folder)
+            mkdir(save_folder)
+        end
+
+        plotname = title
+        save_path = abspath(joinpath(save_folder, plotname * ".png"))
+        # savefig = make_savefig(x, y, plotname, to_value(ax.xlabel), to_value(ax.ylabel))
+        save(save_path, fig)
+        println("Saved figure to ", save_path)
+    end
+    
+    return fig
 end
